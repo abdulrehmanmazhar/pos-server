15a9fce1ade9ffd1ec6c0c7f0ea67ba6895d33ea
abdulrehmanmazhar <abdulrehmanmazhar02@gmail.com>
2024-12-26 16:19:28 +0500

    same-site-none

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -18,14 +18,14 @@ export const accessTokenOptions : ITokenOptions ={
     expires: new Date(Date.now()+ accessTokenExpire+(1000*60*60)),
     maxAge: accessTokenExpire+60*60*1000,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
 }
 
 export const refreshTokenOptions : ITokenOptions ={
     expires: new Date(Date.now()+ refreshTokenExpire+(1000*60*60*24)),
     maxAge: accessTokenExpire+60*60*24*1000,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
 }
 
 export const sendToken = ( user: IUser, statusCode: number, res: Response)=>{
